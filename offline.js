73a1381c5de2d602ed1524b3c85fc93bae32b4d5
Sturmfuhrer88 <145921087+Sturmfuhrer88@users.noreply.github.com>
2025-02-02 16:58:44 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1738504556,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/shipyellow_manned-sheet0.png",
+		"images/shipblue_damage2-sheet0.png",
+		"images/shipyellow_damage-sheet0.png",
+		"images/touchfollowme-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"media/success.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
